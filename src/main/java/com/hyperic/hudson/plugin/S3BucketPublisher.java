c50bede284700224d1c53ae8497d36930b249770
Stefan Zier <stefan@sumologic.com>
2012-08-10 18:02:24 -0700

    Switching hudson-s3 over to the AWS SDK for Java.
    Summary: Attempt to improve the Hudson plugin for S3.
    
    Test Plan: When Hudson is quiet, plug it in.
    
    Reviewers: carlton, gorzell, jacek, sebastian
    
    Differential Revision: https://review.sumologic.net/D86

--- src/main/java/com/hyperic/hudson/plugin/S3BucketPublisher.java
+++ src/main/java/com/hyperic/hudson/plugin/S3BucketPublisher.java
@@ -14,6 +14,9 @@ import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
 import hudson.util.CopyOnWriteList;
 import hudson.util.FormValidation;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.IOException;
 import java.io.PrintStream;
@@ -23,208 +26,200 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import net.sf.json.JSONObject;
-
-import org.apache.commons.lang.StringUtils;
-import org.jets3t.service.S3ServiceException;
-import org.kohsuke.stapler.StaplerRequest;
-
 public final class S3BucketPublisher extends Notifier {
 
-    private String profileName;
-    
-    public static final Logger LOGGER = Logger.getLogger(S3BucketPublisher.class.getName());
+  private String profileName;
 
-    private final List<Entry> entries = new ArrayList<Entry>();
+  public static final Logger LOGGER = Logger.getLogger(S3BucketPublisher.class.getName());
 
-    public S3BucketPublisher() {
+  private final List<Entry> entries = new ArrayList<Entry>();
+
+  public S3BucketPublisher() {
+  }
+
+  public S3BucketPublisher(String profileName) {
+    if (profileName == null) {
+      // defaults to the first one
+      S3Profile[] sites = DESCRIPTOR.getProfiles();
+      if (sites.length > 0)
+        profileName = sites[0].getName();
     }
-        
-    public S3BucketPublisher(String profileName) {
-        if (profileName == null) {
-            // defaults to the first one
-            S3Profile[] sites = DESCRIPTOR.getProfiles();
-            if (sites.length > 0)
-                profileName = sites[0].getName();
+    this.profileName = profileName;
+  }
+
+
+  public List<Entry> getEntries() {
+    return entries;
+  }
+
+  public S3Profile getProfile() {
+    S3Profile[] profiles = DESCRIPTOR.getProfiles();
+    if (profileName == null && profiles.length > 0)
+      // default
+      return profiles[0];
+
+    for (S3Profile profile : profiles) {
+      if (profile.getName().equals(profileName))
+        return profile;
+    }
+    return null;
+  }
+
+  public BuildStepMonitor getRequiredMonitorService() {
+    return BuildStepMonitor.BUILD;
+  }
+
+
+  @Override
+  public boolean perform(AbstractBuild<?, ?> build,
+                         Launcher launcher,
+                         BuildListener listener)
+    throws InterruptedException, IOException {
+
+    if (build.getResult() == Result.FAILURE) {
+      // build failed. don't post
+      return true;
+    }
+
+    S3Profile profile = getProfile();
+    if (profile == null) {
+      log(listener.getLogger(), "No S3 profile is configured.");
+      build.setResult(Result.UNSTABLE);
+      return true;
+    }
+    log(listener.getLogger(), "Using S3 profile: " + profile.getName());
+    try {
+      profile.login();
+    } catch (RuntimeException e) {
+      throw new IOException("Can't connect to S3 service: " + e);
+    }
+
+    try {
+      Map<String, String> envVars = build.getEnvironment(listener);
+
+      log(listener.getLogger(), "Entries: " + entries);
+
+      for (Entry entry : entries) {
+        String expanded = Util.replaceMacro(entry.sourceFile, envVars);
+        FilePath ws = build.getWorkspace();
+        FilePath[] paths = ws.list(expanded);
+
+        if (paths.length == 0) {
+          // try to do error diagnostics
+          log(listener.getLogger(), "No file(s) found: " + expanded);
+          String error = ws.validateAntFileMask(expanded);
+          if (error != null)
+            log(listener.getLogger(), error);
         }
-        this.profileName = profileName;
-    }
-
-    
-	public List<Entry> getEntries() {
-        return entries;
-    }
-        
-    public S3Profile getProfile() {
-        S3Profile[] profiles = DESCRIPTOR.getProfiles();
-        if (profileName == null && profiles.length > 0)
-            // default
-            return profiles[0];
-
-        for (S3Profile profile : profiles) {
-            if (profile.getName().equals(profileName))
-                return profile;
+        String bucket = Util.replaceMacro(entry.bucket, envVars);
+        for (FilePath src : paths) {
+          log(listener.getLogger(), "bucket=" + bucket + ", file=" + src.getName());
+          profile.upload(bucket, src, listener.getLogger());
         }
-        return null;
+      }
+    } catch (IOException e) {
+      e.printStackTrace(listener.error("Failed to upload files"));
+      build.setResult(Result.UNSTABLE);
+    } finally {
+      if (profile != null) {
+        profile.logout();
+      }
     }
 
-    public BuildStepMonitor getRequiredMonitorService() {
-		return BuildStepMonitor.BUILD;
-	}
+    return true;
+  }
 
+  @Override
+  public BuildStepDescriptor<Publisher> getDescriptor() {
+    return DESCRIPTOR;
+  }
+
+  @Extension
+  public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+
+  public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+
+    public DescriptorImpl() {
+      super(S3BucketPublisher.class);
+      load();
+    }
+
+    protected DescriptorImpl(Class<? extends Publisher> clazz) {
+      super(clazz);
+    }
+
+    private final CopyOnWriteList<S3Profile> profiles = new CopyOnWriteList<S3Profile>();
+
+    public String getDisplayName() {
+      return "Publish artifacts to S3 Bucket";
+    }
+
+    public String getShortName() {
+      return "[S3] ";
+    }
 
     @Override
-    public boolean perform(AbstractBuild<?, ?> build,
-                           Launcher launcher,
-                           BuildListener listener)
-        throws InterruptedException, IOException {
-
-        if (build.getResult() == Result.FAILURE) {
-            // build failed. don't post
-            return true;
-        }
-
-        S3Profile profile = getProfile();
-        if (profile == null) {
-            log(listener.getLogger(), "No S3 profile is configured.");
-            build.setResult(Result.UNSTABLE);
-            return true;
-        }
-        log(listener.getLogger(), "Using S3 profile: " + profile.getName());
-        try {
-            profile.login();
-        } catch (S3ServiceException e) {
-            throw new IOException("Can't connect to S3 service: " + e);
-        }
-
-        try {
-            Map<String, String> envVars = build.getEnvironment(listener);
-
-            log(listener.getLogger(), "Entries: "+entries);
-            
-            for (Entry entry : entries) {
-                String expanded = Util.replaceMacro(entry.sourceFile, envVars);
-                FilePath ws = build.getWorkspace();
-                FilePath[] paths = ws.list(expanded);
-
-                if (paths.length == 0) {
-                    // try to do error diagnostics
-                    log(listener.getLogger(), "No file(s) found: " + expanded);
-                    String error = ws.validateAntFileMask(expanded);
-                    if (error != null)
-                        log(listener.getLogger(), error);
-                }
-                String bucket = Util.replaceMacro(entry.bucket, envVars);
-                for (FilePath src : paths) {
-                    log(listener.getLogger(), "bucket=" + bucket + ", file=" + src.getName());
-                    profile.upload(bucket, src, envVars, listener.getLogger());
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace(listener.error("Failed to upload files"));
-            build.setResult(Result.UNSTABLE);
-        } finally {
-            if (profile != null) {
-                profile.logout();
-            }
-        }
-
-        return true;
+    public String getHelpFile() {
+      return "/plugin/s3/help.html";
     }
 
     @Override
-    public BuildStepDescriptor<Publisher> getDescriptor() {
-        return DESCRIPTOR;
+    public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+      return true;
     }
 
-    @Extension
-    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
-        
-    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
-                
-        public DescriptorImpl() {
-            super(S3BucketPublisher.class);
-            load();
-        }
-                
-        protected DescriptorImpl(Class<? extends Publisher> clazz) {
-            super(clazz);
-        }
-
-        private final CopyOnWriteList<S3Profile> profiles = new CopyOnWriteList<S3Profile>();
-                
-        public String getDisplayName() {
-            return "Publish artifacts to S3 Bucket";
-        }
-                
-        public String getShortName()
-        {
-            return "[S3] ";
-        }
-
-        @Override
-        public String getHelpFile() {
-            return "/plugin/s3/help.html";
-        }
-        
-        @Override
-		public boolean isApplicable(Class<? extends AbstractProject> jobType) {
-			return true;
-		}
-
-        @Override
-        public Publisher newInstance(StaplerRequest req, JSONObject formData) {
-            S3BucketPublisher pub = new S3BucketPublisher();
-            req.bindParameters(pub, "s3.");
-            pub.getEntries().addAll(req.bindParametersToList(Entry.class, "s3.entry."));
-            return pub;
-        }
-                
-        public S3Profile[] getProfiles() {
-            return profiles.toArray(new S3Profile[0]);
-        }
-
-        @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) {
-            profiles.replaceBy(req.bindParametersToList(S3Profile.class, "s3."));
-            save();
-            return true;
-        }
-                
-        
-    	public FormValidation doLoginCheck(final StaplerRequest request) {
-    		final String name = Util.fixEmpty(request.getParameter("name"));
-			if (name == null) { // name is not entered yet
-				return FormValidation.ok();
-			}
-			
-			S3Profile profile = new S3Profile(name, request.getParameter("accessKey"), request.getParameter("secretKey"));
-			
-			try {
-                profile.login();
-                profile.check();
-                profile.logout();
-            } catch (S3ServiceException e) {
-                LOGGER.log(Level.SEVERE, e.getMessage());
-                return FormValidation.error("Can't connect to S3 service: " + e.getS3ErrorMessage());
-            }
-            
-			return FormValidation.ok();
-		}
-        
-        
-		
+    @Override
+    public Publisher newInstance(StaplerRequest req, JSONObject formData) {
+      S3BucketPublisher pub = new S3BucketPublisher();
+      req.bindParameters(pub, "s3.");
+      pub.getEntries().addAll(req.bindParametersToList(Entry.class, "s3.entry."));
+      return pub;
     }
 
-    public String getProfileName() {
-        return this.profileName;
+    public S3Profile[] getProfiles() {
+      return profiles.toArray(new S3Profile[0]);
     }
 
-    public void setProfileName(String profileName) {
-        this.profileName = profileName;
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject formData) {
+      profiles.replaceBy(req.bindParametersToList(S3Profile.class, "s3."));
+      save();
+      return true;
     }
 
-    protected void log(final PrintStream logger, final String message) {
-        logger.println(StringUtils.defaultString(DESCRIPTOR.getShortName()) + message);
+
+    public FormValidation doLoginCheck(final StaplerRequest request) {
+      final String name = Util.fixEmpty(request.getParameter("name"));
+      if (name == null) { // name is not entered yet
+        return FormValidation.ok();
+      }
+
+      S3Profile profile = new S3Profile(name, request.getParameter("accessKey"), request.getParameter("secretKey"));
+
+      try {
+        profile.login();
+        profile.check();
+        profile.logout();
+      } catch (RuntimeException e) {
+        LOGGER.log(Level.SEVERE, e.getMessage());
+        return FormValidation.error("Can't connect to S3 service: " + e.getMessage());
+      }
+
+      return FormValidation.ok();
     }
+
+
+  }
+
+  public String getProfileName() {
+    return this.profileName;
+  }
+
+  public void setProfileName(String profileName) {
+    this.profileName = profileName;
+  }
+
+  protected void log(final PrintStream logger, final String message) {
+    logger.println(StringUtils.defaultString(DESCRIPTOR.getShortName()) + message);
+  }
 }
--- src/main/java/com/hyperic/hudson/plugin/S3Profile.java
+++ src/main/java/com/hyperic/hudson/plugin/S3Profile.java
@@ -1,126 +1,126 @@
 package com.hyperic.hudson.plugin;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import hudson.FilePath;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.Map;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.lang.StringUtils;
-import org.jets3t.service.S3Service;
-import org.jets3t.service.S3ServiceException;
-import org.jets3t.service.impl.rest.httpclient.RestS3Service;
-import org.jets3t.service.model.S3Bucket;
-import org.jets3t.service.model.S3Object;
-import org.jets3t.service.security.AWSCredentials;
-
 public class S3Profile {
-    String name;
-    String accessKey;
-    String secretKey;
-    private S3Service s3;
+  String name;
+  String accessKey;
+  String secretKey;
+  private AmazonS3 s3;
 
-    public static final Logger LOGGER =
-        Logger.getLogger(S3Profile.class.getName());
+  public static final Logger LOGGER =
+    Logger.getLogger(S3Profile.class.getName());
 
-    public S3Profile() {
+  public S3Profile() {
 
+  }
+
+  public S3Profile(String name, String accessKey, String secretKey) {
+    this.name = name;
+    this.accessKey = accessKey;
+    this.secretKey = secretKey;
+  }
+
+  public String getAccessKey() {
+    return accessKey;
+  }
+
+  public void setAccessKey(String accessKey) {
+    this.accessKey = accessKey;
+  }
+
+  public String getSecretKey() {
+    return secretKey;
+  }
+
+  public void setSecretKey(String secretKey) {
+    this.secretKey = secretKey;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void login() {
+    if (this.s3 != null) {
+      return;
+    }
+    try {
+      AWSCredentials creds = new BasicAWSCredentials(this.accessKey, this.secretKey);
+      this.s3 = new AmazonS3Client(creds);
+      this.s3.listBuckets();
+    } catch (RuntimeException e) {
+      LOGGER.log(Level.SEVERE, e.getMessage());
+      throw e;
+    }
+  }
+
+  public void check() {
+    this.s3.listBuckets();
+  }
+
+  public void logout() {
+    this.s3 = null;
+  }
+
+  private void ensureBucket(String bucketName) {
+    List<Bucket> buckets = this.s3.listBuckets();
+    for (Bucket bucket : buckets) {
+      if (bucket.getName().equals(bucketName)) {
+        return;
+      }
     }
 
-    public S3Profile(String name, String accessKey, String secretKey) {
-        this.name = name;
-        this.accessKey = accessKey;
-        this.secretKey = secretKey;
+    this.s3.createBucket(bucketName);
+  }
+
+
+  public void upload(String bucketName,
+                     FilePath filePath,
+                     PrintStream logger)
+    throws IOException, InterruptedException {
+
+    if (filePath.isDirectory()) {
+      throw new IOException(filePath + " is a directory");
+    } else {
+      File file = new File(filePath.getName());
+      ensureBucket(bucketName);
+
+      try {
+        long startTime = System.currentTimeMillis();
+
+        log(logger, String.format("START upload of %s to bucket %s", file.getName(), bucketName));
+        s3.putObject(new PutObjectRequest(bucketName, file.getName(), file));
+
+        log(logger, String.format("DONE upload of %s to bucket %s (%,d)",
+          file.getName(), bucketName, System.currentTimeMillis() - startTime));
+      } catch (Exception e) {
+        throw new IOException("put " + file + ": " + e, e);
+      }
     }
+  }
 
-    public String getAccessKey() {
-        return accessKey;
-    }
-
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
-    }
-
-    public String getSecretKey() {
-        return secretKey;
-    }
-
-    public void setSecretKey(String secretKey) {
-        this.secretKey = secretKey;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void login() throws S3ServiceException {
-        if (this.s3 != null) {
-            return;
-        }
-        try {
-            AWSCredentials creds =
-                new AWSCredentials(this.accessKey, this.secretKey);
-            this.s3 = new RestS3Service(creds);
-        } catch (S3ServiceException e) {
-            LOGGER.log(Level.SEVERE, e.getMessage());
-            throw e;
-        }
-    }
-
-    public void check() throws S3ServiceException {
-        this.s3.listAllBuckets();
-    }
-
-    public void logout() {
-        this.s3 = null;
-    }
-
-    private S3Bucket getOrCreateBucket(String bucketName) throws S3ServiceException {
-        S3Bucket bucket = this.s3.getBucket(bucketName);
-        if (bucket == null) {
-            bucket = this.s3.createBucket(new S3Bucket(bucketName));
-        }
-        return bucket;
-    }
-
-    public void upload(String bucketName,
-                       FilePath filePath,
-                       Map<String, String> envVars,
-                       PrintStream logger)
-        throws IOException, InterruptedException {
-
-        if (filePath.isDirectory()) {
-            throw new IOException(filePath + " is a directory");
-        }
-        else {
-            File file = new File(filePath.getName());
-            S3Bucket bucket;
-            try {
-                bucket = getOrCreateBucket(bucketName);
-            } catch (S3ServiceException e) {
-                throw new IOException(bucketName + " bucket: " + e);
-            }
-
-            try {
-                S3Object fileObject = new S3Object(bucket, file.getName());
-                fileObject.setDataInputStream(filePath.read());
-                this.s3.putObject(bucket, fileObject);
-            } catch (Exception e) {
-                throw new IOException("put " + file + ": " + e, e);
-            }
-        }
-    }
-
-    protected void log(final PrintStream logger, final String message) {
-        final String name =
-            StringUtils.defaultString(S3BucketPublisher.DESCRIPTOR.getShortName());
-        logger.println(name + message);
-    }
+  protected void log(final PrintStream logger, final String message) {
+    final String name =
+      StringUtils.defaultString(S3BucketPublisher.DESCRIPTOR.getShortName());
+    logger.println(name + message);
+  }
 }
