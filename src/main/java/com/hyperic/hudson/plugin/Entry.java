06289f23039f876d90c9dba444ef10f3f1c07abc
Stefan Zier <stefan@sumologic.com>
2012-08-12 08:12:57 -0700

    Fixing Entry.toString to display something more useful.

--- src/main/java/com/hyperic/hudson/plugin/Entry.java
+++ src/main/java/com/hyperic/hudson/plugin/Entry.java
@@ -1,14 +1,19 @@
 package com.hyperic.hudson.plugin;
 
 public final class Entry {
-    /**
-     * Destination bucket for the copy. Can contain macros. 
-     */
-    public String bucket;
-    /**
-     * File name relative to the workspace root to upload.
-     * Can contain macros and wildcards.
-     * <p>
-     */
-    public String sourceFile;
+  /**
+   * Destination bucket for the copy. Can contain macros.
+   */
+  public String bucket;
+  /**
+   * File name relative to the workspace root to upload.
+   * Can contain macros and wildcards.
+   * <p/>
+   */
+  public String sourceFile;
+
+  @Override
+  public String toString() {
+    return String.format("%s -> %s", sourceFile, bucket);
+  }
 }
